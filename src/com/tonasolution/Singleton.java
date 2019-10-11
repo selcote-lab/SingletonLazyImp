db9ba75c7e549b41e4c746a10e9b594b3c37818f
zouhaire <zouhaire@idside.de>
2019-10-11 16:05:00 +0100

    added synchronized keyword for static method

--- src/com/tonasolution/Singleton.java
+++ src/com/tonasolution/Singleton.java
@@ -6,7 +6,7 @@ public class Singleton {
 
     private Singleton(){ }
 
-    public static Singleton getInstance(){
+    public static synchronized Singleton getInstance(){
 
         if(uniqueInstance == null){
             uniqueInstance = new Singleton();
